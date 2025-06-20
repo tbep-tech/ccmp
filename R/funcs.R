383bc5e55affaf4ae6f537523e24b8bf1d0550dc
fawda123 <mbafs2012@gmail.com>
2025-06-20 16:28:50 -0400

    minor fixes from JL

--- R/funcs.R
+++ R/funcs.R
@@ -211,7 +211,8 @@ intertidal_plo <- function(curyr = 2020){
       name = factor(name, levels = c('ca1950', as.character(curyr)))
     ) |> 
     dplyr::mutate(
-      Acreslab = formatC(round(Acres, 0), format = 'd', big.mark = ',')
+      Acreslab = formatC(round(Acres, 0), format = 'd', big.mark = ','), 
+      Acreslab = paste0(Acreslab, '\n', HMPU_TARGETS)
     ) |> 
     dplyr::arrange(name, HMPU_TARGETS)
   
@@ -224,7 +225,7 @@ intertidal_plo <- function(curyr = 2020){
     ggplot2::theme_void(base_family = 'Roboto') +
     ggplot2::scale_fill_manual(values = c('#028576', '#427355', '#76923C')) + 
     ggplot2::theme(
-      legend.position = 'bottom', 
+      legend.position = 'none', 
       text = ggplot2::element_text(family = 'Roboto'),
       strip.text = ggplot2::element_text(size = 15), 
       legend.text = ggplot2::element_text(size = 13)
